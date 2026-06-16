e11166721c2ae6692b570cfdddf9e5e28afe94c3
Dagofher <cruzjosecarlos307@gmail.com>
2026-06-15 21:27:47 -0400

    Arreglo de error al entrar a settings

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -16,7 +16,7 @@ class HomePage extends StatelessWidget {
               Navigator.push(
                 context,
                 MaterialPageRoute(
-                  builder: (context) => const DeviceSettingsPage(),
+                  builder: (_) => const DeviceSettingsPage(),
                 ),
               );
             },
--- lib/pages/main.dart
+++ lib/pages/main.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'app.dart';
 
-void main(){
-
-  runApp(TrakioApp());
-  
+void main() {
+  runApp(
+    const ProviderScope(
+      child: TrakioApp(),
+    ),
+  );
 }
